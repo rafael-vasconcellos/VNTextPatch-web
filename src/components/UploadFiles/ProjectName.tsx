9d0ff83cc664c2e5feb1de1c4373dc8c2b3fd7a8
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-07-20 18:51:37 -0300

    Update styling for ProjectName component

--- src/components/UploadFiles/ProjectName.tsx
+++ src/components/UploadFiles/ProjectName.tsx
@@ -6,15 +6,16 @@ export default function ProjectName({ onSubmit }: Props) {
     let input: HTMLInputElement | undefined
 
     return ( 
-        <div class="w-screen h-screen absolute bg-black/50">
-            <div class="bg-black w-48 h-28 px-10">
+        <div class="w-screen h-screen absolute bg-black/5 flex justify-center items-center">
+            <div class="bg-black w-72 px-5 py-2 rounded-xl flex flex-col gap-6">
                 <span class="font-bold">Project name</span>
-                <input type="text" placeholder="Insert project name"
+                <input class="w-full p-2 border-zinc-800 border-2 rounded-lg"
+                 type="text" placeholder="Insert project name"
                  ref={input} onkeyup={(e) => { 
                     if (e.key === "Enter" && onSubmit) { onSubmit(e.currentTarget.value) }
                 }} />
                 <div class="w-full flex justify-end">
-                    <button class="bg-white text-black font-bold rounded-lg" onClick={() => onSubmit(input?.value ?? '')}>
+                    <button class="bg-white text-black font-bold rounded-lg px-4 py-1 mb-4" onClick={() => onSubmit(input?.value ?? '')}>
                         Start project
                     </button>
                 </div>
