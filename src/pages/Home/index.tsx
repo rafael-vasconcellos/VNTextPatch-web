01dcdbc374c6d536ae6e44dc016ab46b9ee3cb25
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-07-20 20:07:26 -0300

    feat: implement local saved projects loading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,13 +5,14 @@ import './style.css'
 import UploadFiles from '../../components/UploadFiles'
 import ProjectName from '../../components/UploadFiles/ProjectName'
 import Explorer from '../Explorer'
+import LocalProjects from '../../components/LocalProjects'
 
 
 export default function App() { 
     const repo = new Repo()
     const [ project_name, setProjectName ] = createSignal('')
     const [ src_files, setSrcFiles ] = createSignal<FileList>()
-    const [ isRepoOpen, setRepoStatus ] = createSignal(false)
+    const [ isProjectOpen, setProjectStatus ] = createSignal(false)
     let fileInput: HTMLInputElement | undefined
 
     createEffect(async() => { 
@@ -22,25 +23,35 @@ export default function App() {
                 outputFiles: await vn.extractLocalAsSheets(src_files()),
                 srcFiles: src_files()
             })
-            setRepoStatus(repo.isOpen)
+            setProjectStatus(repo.isOpen)
         }
     })
 
 
-    return (
-      <section class='w-screen h-screen flex justify-center items-center'>
-        <Show when={!src_files()?.length}>
-            <UploadFiles ref={fileInput} onChange={() => { 
-                if (fileInput?.files?.length) { setSrcFiles(fileInput.files) }
-            }} />
-        </Show>
-        <Show when={src_files()?.length && !project_name()}>
-            <ProjectName onSubmit={(name: string) => setProjectName(name)} />
-        </Show>
-        <Show when={isRepoOpen()}>
-            <Explorer projectName={project_name()} />
-        </Show>
-      </section>
+    return ( 
+        <>
+            <Show when={!isProjectOpen()}>
+                <section class='w-screen h-screen flex flex-col justify-center items-center gap-8'>
+                    <div class='flex flex-col gap-8'>
+                        <Show when={!src_files()?.length && !isProjectOpen()}>
+                            <UploadFiles ref={fileInput} onChange={() => { 
+                                if (fileInput?.files?.length) { setSrcFiles(fileInput.files) }
+                            }} />
+                        </Show>
+                        <LocalProjects onClick={projectName => { 
+                            setProjectName(projectName)
+                            setProjectStatus(true)
+                        } }/>
+                    </div>
+                </section>
+            </Show>
+            <Show when={src_files()?.length && !project_name()}>
+                <ProjectName onSubmit={(name: string) => setProjectName(name)} />
+            </Show>
+            <Show when={isProjectOpen()}>
+                <Explorer projectName={project_name()} />
+            </Show>
+        </>
     )
 }
 
