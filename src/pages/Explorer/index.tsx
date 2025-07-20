c5fb0b6909b1894fb6005de171c9559b29c9a6b8
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-07-20 06:05:37 -0300

    feat: add Explorer component for project management and sheet display

---
+++ src/pages/Explorer/index.tsx
@@ -0,0 +1,46 @@
+import { createEffect, createSignal, For, Show } from "solid-js"
+import Sheet from "../../components/Sheet"
+import { Repo } from "../../global/Repo"
+
+interface ExplorerProps { 
+    projectName: string
+    //srcFiles: FileList
+}
+
+
+export default function Explorer({ projectName }: ExplorerProps) { 
+    const repo = new Repo(projectName)
+    const [ current_file, setCurrentFile ] = createSignal<string>('')
+    const [ project_files, setProjectFiles ] = createSignal<string[]>([])
+    const [ sheet, setSheet ] = createSignal<string[][] | undefined>()
+
+    createEffect(async() => { 
+        repo.open()
+        const files = await repo.getSheets().then(sheets => sheets?.map(store => store.filename))
+        if (files) { 
+            setProjectFiles(files); setCurrentFile(files[0])
+        }
+    })
+
+    createEffect(async() => { 
+        if (current_file()) { 
+            const s = await repo.getSheet(current_file())
+            s && setSheet(s)
+        }
+    })
+
+
+    return ( 
+        <main class="p-14 flex gap-16">
+            <section>
+                <For each={project_files()}>
+                    { fileName => 
+                        <button onClick={() => setCurrentFile(fileName)}>{fileName}</button> }
+                </For>
+            </section>
+            <Show when={sheet()}>
+                <Sheet sheet={sheet() as string[][]} />
+            </Show>
+        </main>
+    )
+}
