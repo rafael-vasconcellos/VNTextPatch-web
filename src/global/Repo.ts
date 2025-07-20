000e63fc4bc2e5fa9b705f6501b2e656fff309c8
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-07-20 06:04:52 -0300

    feat: enhance Repo class with optional srcFiles and outputFiles handling; add getSheets method; enhance typing

--- src/global/Repo.ts
+++ src/global/Repo.ts
@@ -1,81 +1,112 @@
 interface IRepoInit { 
-    projectName: string
-    srcFiles: FileList | null
-    outputFiles: Record<string, string[][]>
+    srcFiles?: FileList | null
+    outputFiles?: Record<string, string[][]>
+}
+
+interface StoreItem { 
+    filename: string
+    content: any
 }
 
 
 export class Repo {  
+    private request?: IDBOpenDBRequest
     public projectName: string
-    private request: IDBOpenDBRequest
-    constructor(init: IRepoInit) { 
-        if (!init.srcFiles) { throw new Error("Repo error: srcFiles can't be null.") }
-        this.projectName = init.projectName
-        this.request = indexedDB.open(this.projectName, 1)
+    constructor(projectName: string) { 
+        this.projectName = projectName
+    }
 
+    open() { 
+        this.request = indexedDB.open(this.projectName, 1)
+        this.request.onerror = () => { console.error(this.request!.error) }
+    }
+
+    create(init?: IRepoInit) { 
+        this.request = indexedDB.open(this.projectName, 1)
         this.request.onupgradeneeded = (event) => { 
             const db = (event.target as IDBOpenDBRequest).result
-            this.create(db, init.outputFiles)
-            this.request.onsuccess = () => this.insertSrcFiles(init.srcFiles as FileList)
+            db.createObjectStore("source_files", { keyPath: "name" })
+            db.createObjectStore("sheets", { keyPath: "filename" })
+            if (init?.outputFiles) { 
+                this.insertSheets(init.outputFiles)
+            }
+            if (init?.srcFiles) { 
+                this.request!.onsuccess = () => this.insertSrcFiles(init.srcFiles as FileList)
+            }
         }
-        this.request.onerror = () => { console.error(this.request.error) }
+        this.request.onerror = () => { console.error(this.request!.error) }
     }
 
-    private db(): IDBDatabase { 
-        return this.request.result
+    private db(): IDBDatabase | undefined { 
+        return this.request?.result
     }
 
-    private create(db: IDBDatabase, outputFiles: Record<string, string[][]>) { 
-        db.createObjectStore("source_files", { keyPath: "name" })
-
-        const store = db.createObjectStore("sheets", { keyPath: "filename" })
+    private insertSheets(outputFiles: Record<string, string[][]>) { 
+        if (!this.db()) { return }
+        const transaction = this.db()?.transaction("sheets", "readwrite")
+        const store = transaction?.objectStore("sheets")
         for (const fileName in outputFiles) { 
-            store.add({ 
+            store?.add({ 
                 filename: fileName,
                 content: outputFiles[fileName]
-            })
+            } as StoreItem)
         }
     }
 
     private async insertSrcFiles(srcFiles: FileList) { 
+        if (!this.db()) { return }
         const files = await Promise.all(Array.from(srcFiles).map(async file => ({ 
-            name: file.name,
+            filename: file.name,
             content: new Uint8Array(await file.arrayBuffer())
-        })))
-        const transaction = this.db().transaction("source_files", "readwrite")
-        const store = transaction.objectStore("source_files")
-        for (const file of files) { store.add(file) }
+        } as StoreItem)))
+        const transaction = this.db()?.transaction("source_files", "readwrite")
+        const store = transaction?.objectStore("source_files")
+        for (const file of files) { store?.add(file) }
     }
 
-    getSourceFiles() { 
+    async getSourceFiles() { 
+        if (!this.db()) { return Promise.resolve() }
         const { promise, resolve, reject } = Promise.withResolvers()
-        const transaction = this.db().transaction("source_files", "readonly")
+        const transaction = this.db()!.transaction("source_files", "readonly")
         const store = transaction.objectStore("source_files")
         const response = store.getAll()
 
         response.onsuccess = () => resolve(response.result)
         response.onerror = () => reject(response.error)
-        return promise
+        return promise as Promise<StoreItem[]>
     }
 
     updateSheet(fileName: string, sheet: string[][]) { 
-        const transaction = this.db().transaction("sheets", "readwrite")
-        const store = transaction.objectStore("sheets")
+        if (!this.db()) { return }
+        const transaction = this.db()?.transaction("sheets", "readwrite")
+        const store = transaction?.objectStore("sheets")
         store?.put({ 
             filename: fileName,
             content: sheet
-        })
+        } as StoreItem)
     }
 
-    getSheet(fileName: string) { 
+    async getSheet(fileName: string) { 
+        if (!this.db()) { return Promise.resolve() }
         const { promise, resolve, reject } = Promise.withResolvers()
-        const transaction = this.db().transaction("sheets", "readwrite")
+        const transaction = this.db()!.transaction("sheets", "readwrite")
         const store = transaction.objectStore("sheets")
-        const index = store.index("filename")
-        const response = index.get(fileName)
+        const response = store.get(fileName)
+
+        response.onsuccess = () => resolve(response.result?.content)
+        response.onerror = () => reject(response.error)
+        return promise as Promise<string[][]>
+    }
+
+    async getSheets() { 
+        if (!this.db()) { return Promise.resolve() }
+        const { promise, resolve, reject } = Promise.withResolvers()
+        const transaction = this.db()!.transaction("sheets", "readwrite")
+        const store = transaction.objectStore("sheets")
+        const response = store.getAll()
 
         response.onsuccess = () => resolve(response.result)
         response.onerror = () => reject(response.error)
-        return promise
+        return promise as Promise<StoreItem[]>
     }
 }
