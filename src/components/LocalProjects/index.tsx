f198648ebf73c1fe3502c87896e622ff95118e16
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-07-20 22:06:30 -0300

    feat: implement saved projects deletion

---
+++ src/components/LocalProjects/index.tsx
@@ -0,0 +1,43 @@
+import { For } from "solid-js"
+import { createEffect, createSignal } from "solid-js"
+import './style.css'
+
+
+interface LocalProjectsProps { 
+    onClick?: (s: string) => void
+}
+
+export default function LocalProjects({ onClick }: LocalProjectsProps) {
+    const [ projects, setProjects ] = createSignal<(string | undefined)[]>([])
+    createEffect(getProjects)
+
+    async function getProjects() { 
+        const dbs = await indexedDB.databases()
+        setProjects(dbs.map(db => db.name))
+    }
+
+    function deleteProject(projectName: string) { 
+        const request = indexedDB.deleteDatabase(projectName)
+        request.onerror = () => console.error(request.error)
+        request.onsuccess = getProjects
+    }
+
+
+    return ( 
+        <For each={projects()}>
+            { projectName => 
+                <button class="p-4 text-xl border-2 border-zinc-800 rounded-lg cursor-pointer relative hover:bg-white hover:text-black" 
+                 onClick={() => onClick && onClick(projectName as string)}>
+                    {projectName}
+                    <svg class="size-6 p-1 text-white bg-red-800 absolute top-0 right-0" stroke="currentColor" stroke-width="1.5" 
+                     onClick={(e) => { 
+                        deleteProject(projectName as string)
+                        e.stopPropagation()
+                    } } xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </button> 
+            }
+        </For>
+    )
+}
