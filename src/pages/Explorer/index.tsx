5ac910cb39aea19dd43486cf290fb3c71503f5c5
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-09-26 05:06:38 -0300

    fix: update UI refreshing after sheet editions

--- src/pages/Explorer/index.tsx
+++ src/pages/Explorer/index.tsx
@@ -11,33 +11,30 @@ interface ExplorerProps {
     projectName?: string
 }
 
-interface ProjectFiles {
-    name: string
-    percentTranslated: number | string
-}
+type ProjectFiles = Record<string, string>
 
 export default function Explorer({  }: ExplorerProps) { 
     const [ repo ] = useRepoContext()
     const [ current_file, setCurrentFile ] = createSignal<string>('')
-    const [ project_files, setProjectFiles ] = createSignal<ProjectFiles[]>([])
+    const [ project_files, setProjectFiles ] = createSignal<ProjectFiles>({})
     const [ sheet, setSheet ] = createSignal<ISheet>()
 
     async function getSheetsData() {
         const sheets = await repo()?.getSheets()
-        const files: ProjectFiles[] = sheets?.map(store => ({
-            name: store.filename,
-            percentTranslated: (((store.translatedRows || 0) / store.rows) * 100).toFixed(2) + "%"
-        }))
+        const files: ProjectFiles = {}
+        sheets?.forEach(store => {
+            files[store.filename] = (((store.translatedRows || 0) / store.rows) * 100).toFixed(2) + "%"
+        })
         return { sheets, files }
     }
 
     createEffect(async() => { //console.log(repo())
         repo()?.open()
         const { sheets, files } = await getSheetsData()
-        if (files.length) { 
+        if (Object.keys(files).length) { 
             setProjectFiles(files)
             setSheet(sheets[0])
-            setCurrentFile(files[0].name)
+            setCurrentFile(sheets[0].filename)
         }
 
         repo()?.addEventListener<"sheetimport">("sheetimport", async() => {
@@ -60,20 +57,23 @@ export default function Explorer({  }: ExplorerProps) {
             <MenuBar />
             <main class="px-8 py-14 flex gap-8">
                 <section class="h-fit flex flex-col rounded-xl border-primary border-x-3 border-y-[20px]">
-                    <For each={project_files()}>
-                        { file => { 
-                            const color = () => current_file() === file.name? "bg-primary text-white border-[0px]" : "bg-handsontable-background text-handsontable-foreground border-y-[1px]"
+                    <For each={Object.keys(project_files())}>
+                        { fileName => { 
+                            const color = () => current_file() === fileName? "bg-primary text-white border-[0px]" : "bg-handsontable-background text-handsontable-foreground border-y-[1px]"
                             return <button class={`px-3 py-2 cursor-pointer ${color()} border-handsontable-border font-handsontable text-left flex justify-between items-center`}
-                                    onClick={() => setCurrentFile(file.name)}>
-                                        {file.name}
-                                        <span>{file.percentTranslated}</span>
+                                    onClick={() => setCurrentFile(fileName)}>
+                                        {fileName}
+                                        <span>{project_files()[fileName]}</span>
                                     </button>
                         }}
                     </For>
                 </section>
                 <Show when={sheet()}>
-                    <Sheet sheet={sheet()} onChange={sheet => { 
-                        repo().updateSheet(current_file(), sheet)
+                    <Sheet sheet={sheet()} onChange={async sheet => { 
+                        const data = await repo().updateSheet(current_file(), sheet)
+                        setSheet(data)
+                        project_files()[current_file()] = (((data.translatedRows || 0) / data.rows) * 100).toFixed(2) + "%"
+                        setProjectFiles({ ...project_files() })
                     }} />
                 </Show>
             </main>
