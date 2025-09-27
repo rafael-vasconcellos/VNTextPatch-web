47ddc48e9446c9a04acac685dc4a924ea46dd74c
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-09-27 03:28:09 -0300

    Update home menubar icon

--- src/components/MenuBar/New.tsx
+++ src/components/MenuBar/New.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from '@solidjs/router'
 import type { JSX } from 'solid-js'
 
 
-export default function New({ class: className }: JSX.ButtonHTMLAttributes<HTMLButtonElement>) { 
+export default function Home({ class: className }: JSX.ButtonHTMLAttributes<HTMLButtonElement>) { 
     const navigate = useNavigate()
     function home() { 
         navigate("/")
@@ -12,7 +12,7 @@ export default function New({ class: className }: JSX.ButtonHTMLAttributes<HTMLB
         <button class={`${className || 'size-8'} cursor-pointer group`} onclick={home}>
             <svg class="text-primary group-hover:text-primary/60" stroke="currentColor" stroke-width="1.5"
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
         </button>
     )
