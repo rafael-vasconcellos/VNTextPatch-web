b9304f745640d8e5a7915458e9a7a3ae8d7e1026
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2025-10-09 16:10:37 -0300

    feat: implement translation configuration and core translation logic

---
+++ src/global/Translator/config.ts
@@ -0,0 +1,57 @@
+export interface TranslatorEngine {
+    translate(texts: (string | null)[]): Promise<string[]>
+    batchSize: number
+}
+
+export class TranslationConfig { 
+    public static get ignoreTranslated(): boolean {
+        return Boolean(sessionStorage.getItem("ignoreTranslated")) ?? true
+    }
+
+    public static get overrideCells(): boolean {
+        return Boolean(sessionStorage.getItem("overrideCells")) ?? true
+    }
+
+    public static get saveOnEachBatch(): boolean {
+        return Boolean(sessionStorage.getItem("saveOnEachBatch")) ?? true
+    }
+
+    public static get targetColumn(): number {
+        return Number(sessionStorage.getItem("targetColumn")) ?? 1
+    }
+
+    public static get srcColumn(): number {
+        return Number(sessionStorage.getItem("srcColumn")) ?? 1
+    }
+
+
+
+
+    public static setIgnoreTranslated(newValue: boolean) {
+        sessionStorage.setItem("ignoreTranslated", String(newValue))
+    }
+
+    public static setOverrideCells(newValue: boolean) {
+        sessionStorage.setItem("overrideCells", String(newValue))
+    }
+
+    public static setSaveOnEachBatch(newValue: boolean) {
+        sessionStorage.setItem("saveOnEachBatch", String(newValue))
+    }
+
+    public static setTargetColumn(newValue: number) {
+        sessionStorage.setItem("targetColumn", String(newValue))
+    }
+
+    public static setSrcColumn(newValue: number) {
+        sessionStorage.setItem("srcColumn", String(newValue))
+    }
+
+    public static setTranslatorName(newValue: string) {
+        sessionStorage.setItem("translatorName", newValue)
+    }
+
+    public static get translatorName() {
+        return sessionStorage.getItem("translatorName") ?? "DeepLX"
+    }
+}
---
+++ src/global/Translator/index.ts
@@ -0,0 +1,62 @@
+import { TranslationConfig, type TranslatorEngine } from "./config";
+import { updateSheet } from "../utils"
+import type { ProjectRepo, Sheet } from "../ProjectRepo";
+
+
+
+interface TranslatorInit {
+    engine: TranslatorEngine
+    repo: ProjectRepo
+    sheetNames: string[]
+}
+
+export class Translator { 
+    private readonly repo: ProjectRepo
+    private readonly engine: TranslatorEngine
+    private readonly sheetNames: string[]
+    constructor(init: TranslatorInit) {
+        this.engine = init.engine
+        this.repo = init.repo
+        this.sheetNames = init.sheetNames
+    }
+
+    async translate() {
+        for (const sheetName of this.sheetNames) {
+            const sheet = await this.repo.getSheet(sheetName)
+            this.translateSheet(sheet)
+            updateSheet(sheet.filename, sheet.content)
+        }
+    }
+
+    async translateSheet(sheet: Sheet) {
+        for (let startIndex=0; startIndex<sheet.content.length; startIndex+=this.engine.batchSize) {
+            const endIndex = startIndex + this.engine.batchSize
+            const slice: {
+                index: number
+                row: (string | null)[]
+            }[] = []
+            for (let j=startIndex; j<Math.min(endIndex, sheet.content.length); j++) {
+                if (!sheet.content[j]) break
+                if (sheet.content[j][TranslationConfig.targetColumn] && TranslationConfig.ignoreTranslated) 
+                    continue
+                slice.push({
+                    index: j,
+                    row: sheet.content[j]
+                })
+            }
+
+
+            if (slice.length === 0) continue
+            const batch = slice.map(item => item.row[TranslationConfig.srcColumn])
+            const translation = await this.engine.translate(batch)
+            translation.forEach((line, i) => {
+                if (slice[i].row[TranslationConfig.targetColumn] && !TranslationConfig.overrideCells) return
+                slice[i].row[TranslationConfig.targetColumn] = line
+                sheet.content[slice[i].index] = slice[i].row
+            })
+
+            if (TranslationConfig.saveOnEachBatch) { this.repo.updateSheet(sheet) }
+        }
+    }
+
+}
